69f06b4068d6653c9d8928b82bed41356c802653
agus <agcasas@itba.edu.ar>
2022-04-28 16:12:08 -0300

    a

--- main.cpp
+++ main.cpp
@@ -6,8 +6,22 @@ int main(char argc, char* argv[]) {
 
 	ifstream fileIn1(argv[1]);
 	string linea, genoma1, genoma2;
-	do(getline(fileIn1, linea)){
+	do(getline(fileIn1, linea)); {
 		while(!linea.find("ORIGIN"))
     }
+	//busco el numero
+	//leo 6 strings y los almaceno en genoma1
+	//l
+	genoma1 += genoma1 tiraADN
 	return 0;
 }
+
+/*
+		TESTS:
+
+-Verificacion del archivo
+-Verificacion del guardado de strings
+-Verificacion del valor de coincidencia
+-Verificacion del resultado(dos genomas alineados)
+
+*/
