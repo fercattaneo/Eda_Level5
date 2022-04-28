e4e711885fd20a3921beb4a8614a37bb4fb59bd7
agus <agcasas@itba.edu.ar>
2022-04-28 14:27:20 -0300

    a

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-int main() {
+#include <fstream>
+#include <string>
 
+using namespace std;
+int main(char argc, char* argv[]) {
 
+	ifstream fileIn1(argv[1]);
+	string linea, genoma1, genoma2;
+	do(getline(fileIn1, linea)){
+		while(!linea.find("ORIGIN"))
+    }
 	return 0;
 }
