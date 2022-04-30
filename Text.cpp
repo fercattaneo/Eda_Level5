60ede0dd3b038a783b09d03788de05f7fe9f1d45
agus <agcasas@itba.edu.ar>
2022-04-30 19:30:41 -0300

    getGenoma 1era Version

--- Text.cpp
+++ Text.cpp
@@ -1,38 +1,57 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <iostream>
 
 using namespace std;
 
 
-string getGenoma(char* file) {
+string getGenoma(char* file, string& genoma) {
 	ifstream fileIn1(file);
-	string linea, genoma1, genoma2;
+	string linea;
 	size_t found = string::npos;
 	long int geneNumber = 0;
-	while ((getline(fileIn1, linea)) && (found == string::npos))
+
+	//la evaluacion es lazy asi que en el caso que se encuentre ORIGIN, no se evalua el siguiente getline
+	while ((found == string::npos) && (getline(fileIn1, linea)))
 	{
 		found = linea.find("ORIGIN");
-
 	}
 	//si el archivo contiente ORIGIN...
 	if (found != string::npos)
 	{
-		//busco el numero 1 para empezar a llenar el genoma
 		found = string::npos;
 
-		while ((getline(fileIn1, linea)) && (found == string::npos))
+		//busco el numero 1 para empezar a llenar el genoma
+		while ((found == string::npos) && (getline(fileIn1, linea)))
 		{
 			found = linea.find("1");
-
 		}
+
+		found = string::npos;
+
+		/*
+		*leo 6 strings y los almaceno en genoma. Paro cuando encuentro la doble barra o 
+		* se termina el archivo
+		*/
 		do
 		{
+			found = linea.find("//");
 
-		} while
+			istringstream iss(linea);
+			iss >> geneNumber;
+			cout << "Numero de caracter inicial: " << geneNumber << endl;
+
+			int i = 0;
+			string palabraAux;
+
+			//leo un maximo de 6 strings. Si hay menos strings paro de leer
+			while ((i < 6) && (iss >> palabraAux))
+			{
+				cout << "String Agregado al genoma: " << palabraAux;
+				genoma += palabraAux;
+			}
+
+		} while (getline(fileIn1, linea) && (found == string::npos));
 	}
-	//busco el numero
-	//leo 6 strings y los almaceno en genoma1
-	//l
-	genoma1 += tiraADN;
 }
