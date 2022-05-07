1cd0ef24d4270aec596f9abfc4960c67838373fe
agus <agcasas@itba.edu.ar>
2022-05-07 18:34:27 -0300

    a

--- logic.cpp
+++ logic.cpp
@@ -9,7 +9,7 @@ int useAlgoritm(string& genoma1, string& genoma2) {
 	//la matriz sera de m+1 x n+1 ; con m = size del genoma1, n= size del genoma2
 	vector < vector <casilla_t> > matrix;
 	matrix[0].push_back({ 0,{0,0,0,0} });
-}
+
 
 	//Otra forma de hacerlo
 	casilla_t* mat2 = new casilla_t[(genoma1.size() + 1) * (genoma2.size() + 1)];
@@ -21,6 +21,11 @@ int useAlgoritm(string& genoma1, string& genoma2) {
 	for (int i = 1; i < genoma1.size() + 1; i++)
 	{
 		matrix[i].push_back({ -i,{0,0,0,0} });
+	}
+	for (auto& i : matrix) {
+		for (auto j : i) {
+			i[1]
+		}
 	}
 	//cout << matrix;
 	return 0;
